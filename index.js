1cd809e37e64f21ef1ddb3e0af1b886a04ecb80c
Hassan <pkhajeh@gmail.com>
2022-06-26 03:49:11 +0200

    register

--- index.js
+++ index.js
@@ -239,13 +239,13 @@ app.post("/register", async (req, res) => {
 
   
 
-  let { id } = await User.findOne({ username: username });
-  mailer(
-    username,
-    "Welcome to Iranian SE",
-    "Yes you are very welcome now \n please activate ur account by clicking this link\n \n http://iranianse.com/activate/" +
-      id
-  ); //Detta lokal host ska ändras till domänen
+  // let { id } = await User.findOne({ username: username });
+  // mailer(
+  //   username,
+  //   "Welcome to Iranian SE",
+  //   "Yes you are very welcome now \n please activate ur account by clicking this link\n \n http://iranianse.com/activate/" +
+  //     id
+  // ); //Detta lokal host ska ändras till domänen
 
   // req.session.user_id = user._id;
   // let { id } = await User.findOne({ username: username });
